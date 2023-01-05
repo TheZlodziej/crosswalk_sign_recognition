1b3ea8d097df58701a678ebaafa78e75057446ab
Jakub Wicher <galaxykuby@gmail.com>
2023-01-05 10:37:08 +0100

    cleanup

--- classification.py
+++ classification.py
@@ -1,37 +1,24 @@
 import tensorflow as tf
 import numpy as np
+import glob
 
-# Load the model
 model = tf.keras.models.load_model('model.h5')
 
-# Create an empty list to store the predictions
-predictions = []
+img_paths = glob.glob('./predict_images/*.jpg')
 
-# Loop over the input images
-input_imgs = ['predict_images/pred0.jpg', 'predict_images/pred1.jpg', 'predict_images/pred2.jpg', 'predict_images/pred3.jpg']
-for image_path in input_imgs:
-  # Read the image file and resize it
-  img = tf.keras.preprocessing.image.load_img(image_path, target_size=(150, 150))
+# img_paths = ['predict_images/pred0.jpg', 'predict_images/pred1.jpg',
+#              'predict_images/pred2.jpg', 'predict_images/pred3.jpg']
 
-  # Convert the image to a NumPy array
-  input_array = tf.keras.preprocessing.image.img_to_array(img)
+images = [tf.keras.preprocessing.image.load_img(
+    img_path, target_size=(150, 150)) for img_path in img_paths]
 
-  # Add an additional dimension to the array (since the model expects a batch of images, not a single image)
-  input_array = np.expand_dims(input_array, axis=0)
+images = [tf.keras.preprocessing.image.img_to_array(image) for image in images]
 
-  # Use the model to make a prediction
-  prediction = model.predict(input_array)
+images = np.array(images)
 
-  # Append the prediction to the list
-  predictions.append(prediction)
+predictions = model.predict(images)
+predictions = (predictions > 0.5).astype(int)
 
-# Print results
-result = []
-for i, entry in enumerate(predictions):
-    # od 80% w gore
-    if entry[0][0] + 0.2 >= 1:
-        result.append(f"{input_imgs[i]} is a crosswalk sign")
-    else:
-        result.append(f"{input_imgs[i]} is not a crosswalk sign")
-
-print("\n".join(result))
+for i in range(len(predictions)):
+    print(
+        f'{img_paths[i]} is {"not" if not predictions[i][0] else ""} a crosswalk sign')
